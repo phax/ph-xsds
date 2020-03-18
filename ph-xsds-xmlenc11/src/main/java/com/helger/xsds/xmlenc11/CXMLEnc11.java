4129fa246056d0dd2713a43cfe7bc9f5b40333b2
Philip Helger <philip@helger.com>
2020-03-18 21:13:46 +0100

    XMLEnc 1.1

---
+++ ph-xsds-xmlenc11/src/main/java/com/helger/xsds/xmlenc11/CXMLEnc11.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2016-2020 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.xsds.xmlenc11;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.helger.commons.annotation.ReturnsMutableCopy;
+import com.helger.commons.collection.impl.CommonsArrayList;
+import com.helger.commons.collection.impl.ICommonsList;
+import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.xsds.xmldsig.CXMLDSig;
+import com.helger.xsds.xmlenc.CXMLEnc;
+
+/**
+ * Utility class for this schema module
+ *
+ * @author Philip Helger
+ */
+@Immutable
+public final class CXMLEnc11
+{
+  public static final String DEFAULT_PREFIX = "dsig11";
+  public static final String NAMESPACE_URI = "http://www.w3.org/2009/xmlenc11#";
+
+  private CXMLEnc11 ()
+  {}
+
+  /**
+   * @return A list of all includes in the correct order. Never
+   *         <code>null</code>.
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  public static ICommonsList <ClassPathResource> getAllXSDIncludes ()
+  {
+    return new CommonsArrayList <> (CXMLDSig.getXSDResource (), CXMLEnc.getXSDResource ());
+  }
+
+  // Note: requires XMLDSig 1.0 schema
+  // Note: requires XMLEnc 1.0 schema
+  @Nonnull
+  public static final ClassPathResource getXSDResource ()
+  {
+    return new ClassPathResource ("/schemas/xenc-schema-11.xsd", CXMLEnc11.class.getClassLoader ());
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public static ICommonsList <ClassPathResource> getAllXSDResources ()
+  {
+    final ICommonsList <ClassPathResource> ret = getAllXSDIncludes ();
+    ret.add (getXSDResource ());
+    return ret;
+  }
+}
---
+++ ph-xsds-xmlenc11/src/test/java/com/helger/xsds/xmlenc11/CXMLEnc11Test.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2016-2020 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.xsds.xmlenc11;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.validation.Schema;
+
+import org.junit.Test;
+
+import com.helger.commons.collection.impl.ICommonsList;
+import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.xml.schema.XMLSchemaCache;
+import com.helger.xsds.xmlenc11.CXMLEnc11;
+
+/**
+ * Test class for class {@link CXMLEnc11}.
+ *
+ * @author Philip Helger
+ */
+public final class CXMLEnc11Test
+{
+  @Test
+  public void testBasic ()
+  {
+    assertNotNull (CXMLEnc11.getXSDResource ());
+    assertTrue (CXMLEnc11.getXSDResource ().exists ());
+    assertEquals (CXMLEnc11.getXSDResource (), CXMLEnc11.getXSDResource ());
+    assertNotSame (CXMLEnc11.getXSDResource (), CXMLEnc11.getXSDResource ());
+  }
+
+  @Test
+  public void testSchemaCreation ()
+  {
+    final ICommonsList <ClassPathResource> aList = CXMLEnc11.getAllXSDIncludes ();
+    aList.add (CXMLEnc11.getXSDResource ());
+
+    final Schema aSchema = XMLSchemaCache.getInstance ().getFromCache (aList);
+    assertNotNull (aSchema);
+  }
+
+  @Test
+  public void testSchemaCreation2 ()
+  {
+    final ICommonsList <ClassPathResource> aList = CXMLEnc11.getAllXSDResources ();
+
+    final Schema aSchema = XMLSchemaCache.getInstance ().getFromCache (aList);
+    assertNotNull (aSchema);
+  }
+}
